a435df830bdd409a417081e2819815150165d535
raeja0 <raeja@ssfs.org>
2022-11-01 10:31:22 -0400

    Tried, failed. tried again, we're winning.

--- assignment_four.py
+++ assignment_four.py
@@ -26,19 +26,22 @@ def multiplication(num1, num2, user_response):
         print("Oops... Better luck next time")
     return addition_answer
 
-def type(equation):
-    if equation == addition :
-        addition()
-    elif equation == subtraction:
-        subtraction()
+def type(equation, num1, num2, user_response):
+    if equation == "addition":
+        addition(num1, num2, user_response)
+    elif equation == "subtraction":
+        subtraction(num1, num2, user_response)
     else:
-        multiplication()
+        multiplication(num1, num2, user_response)
 
-def main():
-    equation = input("Would you like to try addition, subtraction, or multiplication")
+def main(user_response):
+    equation = str(input("Would you like to try addition, subtraction, or multiplication"))
     max_num = int(input("What is the largest number you would like to use within your equation?"))
     min_num = int(input("What is the smallest number you would like to use within your equation?"))
     num1 = random.randint(min_num, max_num)
     num2 = random.randint(min_num, max_num)
+    print(type(equation, num1, num2, user_response))
     user_response = int(input("Answer >> "))
     return num1, num2, equation, user_response
+
+main(user_response=4)
