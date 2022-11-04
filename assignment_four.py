2e0752cabcbb678fa23a58a240d7a10ed36efcb7
raeja0 <raeja@ssfs.org>
2022-11-04 17:15:51 -0400

    fixed code saying nothing after finishing.

--- assignment_four.py
+++ assignment_four.py
@@ -1,47 +1,44 @@
 import random
-def addition(num1, num2, user_response):
+def addition(num1, num2):
     print((num1),"+",(num2))
     addition_answer = num1 + num2
+    user_response = int(input("Answer >> "))
     if user_response == addition_answer:
-        print("That's correct!")
+        return "That's correct!"
     else:
-        print("Oops... Better luck next time")
-    return addition_answer
+        return "Oops... Better luck next time"
 
-def subtraction(num1, num2, user_response):
-    print((num1), "-", (num2))
+def subtraction(num1, num2):
+    print(num1, "-", num2)
     addition_answer = num1 - num2
+    user_response = int(input("Answer >> "))
     if user_response == addition_answer:
-        print("That's correct!")
+        return "That's correct!"
     else:
-        print("Oops... Better luck next time")
-    return addition_answer
+        return "Oops... Better luck next time"
 
-def multiplication(num1, num2, user_response):
+def multiplication(num1, num2):
     print((num1), "*", (num2))
     addition_answer = num1 * num2
+    user_response = int(input("Answer >> "))
     if user_response == addition_answer:
-        print("That's correct!")
+        return "That's correct!"
     else:
-        print("Oops... Better luck next time")
-    return addition_answer
-
-def type(equation, num1, num2, user_response):
-    if equation == "addition":
-        addition(num1, num2, user_response)
+        return "Oops... Better luck next time"
+def type(equation, num1, num2):
+    if equation == "multiplication":
+        print(multiplication(num1, num2))
     elif equation == "subtraction":
-        subtraction(num1, num2, user_response)
+        print(subtraction(num1, num2))
     else:
-        multiplication(num1, num2, user_response)
+        print(addition(num1, num2))
 
-def main(user_response):
+def main():
     equation = str(input("Would you like to try addition, subtraction, or multiplication"))
     max_num = int(input("What is the largest number you would like to use within your equation?"))
     min_num = int(input("What is the smallest number you would like to use within your equation?"))
     num1 = random.randint(min_num, max_num)
     num2 = random.randint(min_num, max_num)
-    print(type(equation, num1, num2, user_response))
-    user_response = int(input("Answer >> "))
-    return num1, num2, equation, user_response
+    type(equation, num1, num2)
 
-main(user_response=4)
+main()
